50ee1778c1d88430c42b5cfd74d2d527c9a6a7ca
Frederico Munoz <fsmunoz@gmail.com>
2013-06-15 11:41:54 +0100

    Refactoring through the addition of more functions to make it less monolithic and more functionalesque

--- src/oradcn_demo/core.clj
+++ src/oradcn_demo/core.clj
@@ -2,8 +2,8 @@
   (:gen-class)
   (:require [clojure.java.jdbc :as j]
             [clojure.java.jdbc.sql :as s]
-            [clojure.contrib.command-line :as ccl])
-  (:use [clojure.tools.cli :only [cli]])
+            [clojure.contrib.command-line :as ccl]
+            [clojure.tools.cli :refer [cli]])
   (:import java.util.Properties
            oracle.jdbc.OracleConnection
            oracle.jdbc.dcn.DatabaseChangeEvent
@@ -12,55 +12,73 @@
            oracle.jdbc.OracleStatement
            java.util.Arrays))
 
-(defn -main
-  ;;    "Oracle Database Change Notification JDBC demo"
-  [& args]
-  ;; work around dangerous default behaviour in Clojure (NB: this comment is from leiningen)
-  (alter-var-root #'*read-eval* (constantly false))
-  
-  (let [[options args banner] (cli args
-                                   ["-h" "--host"  "Database host"]
-                                   ["-p" "--port" "The port to connect to" :default 1521 :parse-fn #(Integer. %)]
-                                   ["-u" "--user"  "Username"]
-                                   ["-p" "--password" "Password"]
-                                   ["-d" "--db" "Database"]
-                                   ["-l" "--local-port" "The local port that will be used for the callback" :default 6666]
-                                   ["-q" "--query" "The query used for registration" :default "select * from DEMO_CUSTOMERS"]
-                                   ["-h" "--help" "Show help" :default false :flag true])
-        ;; DB Connection spec
-        db {:classname "oracle.jdbc.driver.OracleDriver" ;must be in the path
-            :subprotocol "oracle"
-            :subname (str "thin:@" (:host options) ":" (:port options) ":" (:db options) )
-            :user (:user options)
-            :password (:password options)}
-        prop (new Properties)]   ; Holds the connection properties
-    
-
-    (when (:help options)  ; Help handling
+(defn- parse-arguments 
+  "Parses command-line arguments ARGS and handles --help; returns a vector [options args banner]"
+  [args]
+  (let [[options args banner] 
+        (cli args
+             ["-h" "--host"  "Database host"]
+             ["-p" "--port" "The port to connect to" :default "1521"]
+             ["-u" "--user"  "Username"]
+             ["-p" "--password" "Password"]
+             ["-s" "--sid" "Database SID"]
+             ["-l" "--local-port" "The local port that will be used for the callback" :default "6666"]
+             ["-q" "--query" "The query used for registration" :default "select * from DEMO_CUSTOMERS"]
+             ["-h" "--help" "Show help" :default false :flag true])]
+    ;; Handle --help
+    (when (:help options)  
       (println banner)
       (System/exit 0))
-    
-    ;; Obtain connection, with some debug output
-    (print "> Connecting... ")
-    (try 
-      (do
-        (def conn (j/get-connection db))
-        (println " connected."))
-      (catch Exception e (do
-                           (println "Connection failed")
-                           (println e)
-                           (System/exit 1))))
-    
-    ;; Set the connection options, feel free to play around with the hardcoded ones
+    [options args banner]))
+
+
+
+(defn- create-db-spec 
+  "Returns a properly formated db spec - see clojure.java.jdbc/get-connection documentation"
+  [host port sid user password]
+  {:classname "oracle.jdbc.driver.OracleDriver" ; must be in the path
+   :subprotocol "oracle"
+   :subname (str "thin:@" host ":" port ":" sid)
+   :user user
+   :password password})
+
+(defn- set-notification-options 
+  "Set the connection options, returns a property object with the selected options"
+  [options]
+  (let [prop (new Properties)]
     (doto prop
       (.setProperty OracleConnection/DCN_NOTIFY_ROWIDS "true")
       (.setProperty OracleConnection/DCN_QUERY_CHANGE_NOTIFICATION "true")
       (.setProperty OracleConnection/NTF_LOCAL_TCP_PORT (:local-port options)))
-    
-    ;; Create the notification register with the correct option on the right connection
-    ;; For this notify priviledges are needed, otherwise SQLException ORA-29972
-    (def dcr (.registerDatabaseChangeNotification conn prop))
-    
+    prop))
+
+(defn- set-registration-statement 
+  "Returns a statement that is linked to the registratation"
+  [conn dcr]
+  (let [stmt (.createStatement conn)]
+    (.setDatabaseChangeRegistration stmt dcr) ; add the registration to the statement
+  stmt))
+
+(defn- add-shutdown-hook
+  "House-keeping on Control-C: unregister the DCN from the connection"
+  [conn dcr]
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. (fn [] 
+                               (.unregisterDatabaseChangeNotification conn dcr)
+                               (.close conn)))))
+(defn -main
+  "Oracle Database Change Notification JDBC demo"
+  [& args]
+  ;; work around dangerous default behaviour in Clojure (NB: this comment is from leiningen)
+  (alter-var-root #'*read-eval* (constantly false))
+
+  (let [[options args banner] (parse-arguments args)
+        db (create-db-spec (:host options) (:port options) (:sid options) (:user options) (:password options))
+        conn (j/get-connection db)                           ; the connection to the database
+        prop (set-notification-options options)              ; properties used to specify registration options
+        dcr (.registerDatabaseChangeNotification conn prop)  ; create the notification register with the correct option on the right connection
+        stmt (set-registration-statement conn dcr)           ; this statement is "linked" to to registration
+        rs (.executeQuery stmt (:query options))]            ; specify the tables by using a query
     
     ;; Add the listener - using reify but proxy works as well of course
     (.addListener dcr
@@ -68,26 +86,18 @@
                     (onDatabaseChangeNotification [_ evt]
                       (println (.toString evt)))))
     
-    (def stmt (.createStatement conn)) ; create a new statement
-    (.setDatabaseChangeRegistration stmt dcr) ; add the registration to it 
+    ;; Add a shutdown hook to remove registration and close connection
+    (add-shutdown-hook conn dcr)
     
-    (def rs (.executeQuery stmt (:query options))) ; specify the tables by using a query
     (while (.next rs)) ; Loop through the results - all examples do this, although it works without it. Leaving it as black magic.
     
     (.close rs)
     (.close stmt)
-    
-    ;; Some more debug output
-    (println "> Registered Tables: " (Arrays/toString (.getTables dcr)))
-    (println "> DCN Registration ID: " (.getRegistrationId dcr))
-    (println "> Waiting for DCN events...")
-    
+
     ;; The main thread only exits after the JDBC connection is closed, so no need for a run loop
     ;; It will keep on listening until Control-C is pressed 
     
-    ;; Close the registration on interrupt
-    (.addShutdownHook (Runtime/getRuntime)
-                      (Thread. (fn [] 
-                                 (print "Shutting down...")
-                                 (.unregisterDatabaseChangeNotification conn dcr)
-                                 (.close conn))))))
+    ;; Debug output
+    (println "> Registered Tables: " (Arrays/toString (.getTables dcr)))
+    (println "> DCN Registration ID: " (.getRegistrationId dcr))
+    (println "> Waiting for DCN events...")))
